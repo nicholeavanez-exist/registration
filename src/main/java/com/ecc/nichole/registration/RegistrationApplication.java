470740258ad3686ab9868884572d19dcf17cedc4
nchlnnmrvnz <avanez.nichole@gmail.com>
2024-09-27 11:16:05 +0800

    initial commit

---
+++ src/main/java/com/ecc/nichole/registration/RegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ecc.nichole.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationApplication.class, args);
+	}
+
+}
